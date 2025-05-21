786abff87e5071efc3a02c7ea8a74061edbef663
Evandro-Prog <evandro.presenci@gmail.com>
2025-05-21 12:04:22 -0300

    Manipulação de Imagens
    Integração com o Cosmic,
    Criação do service de upload de imagens para o cosmic
    Criação da logica de todo o service.
    Correção em bugs no upload de imagens

--- devagram/pages/api/createUser.ts
+++ devagram/pages/api/createUser.ts
@@ -4,47 +4,63 @@ import type { StandardResponse } from '../../types/StandardResponse';
 import type { UserRequest } from '../../types/UserRequest';
 import { UserModel } from '../../models/UserModel';
 import md5 from 'md5';
+import { upload, uploadImageCosmic } from '../../services/uploadImageCosmic';
+import nc from 'next-connect';
 
 
-const createUser = async (
-    req: NextApiRequest,
-    res: NextApiResponse<StandardResponse>
-) => {
+const handler = nc()
+    .use(upload.single('avatar'))
+    .post(async (
+        req: NextApiRequest,
+        res: NextApiResponse<StandardResponse>
+    ) => {
 
-    if (req.method === 'POST') {
-        const user = req.body as UserRequest;
+        try {
+            const user = req.body as UserRequest;
 
-        if (!user.name || user.name.length < 3) {
-            return res.status(400).json({ error: 'Nome é obrigatório e precisa ter ao menos 3 caracteres.' });
+            if (!user.name || user.name.length < 3) {
+                return res.status(400).json({ error: 'Nome é obrigatório e precisa ter ao menos 3 caracteres.' });
+            }
+
+            if (!user.email || user.email.length < 5
+                || !user.email.includes('@')
+                || !user.email.includes('.')) {
+                return res.status(400).json({ error: 'O email é obrigatório e precisa ter ao menos 6 caracteres e conter @ e .' });
+            }
+
+            if (!user.password || user.password.length < 6) {
+                return res.status(400).json({ error: 'A senha é obrigatória e precisa ter ao menos 6 caracteres.' });
+            }
+
+            const userExist = await UserModel.findOne({ email: user.email })
+
+            if (userExist) {
+                return res.status(400).json({ error: 'Já existe um usuário cadastrado com esse email.' });
+            }
+
+            const image = await uploadImageCosmic(req);            
+
+            const userToCreate = {
+                name: user.name,
+                email: user.email,
+                password: md5(user.password),
+                avatar: image?.media?.url
+            }
+
+            await UserModel.create(userToCreate);
+            return res.status(200).json({ msg: 'Usuário criado com sucesso!' });
+
+        } catch (error) {
+
+            console.log(error);
+            return res.status(500).json({ error: 'Erro interno do servidor' });
         }
+    });
 
-        if (!user.email || user.email.length < 5
-            || !user.email.includes('@')
-            || !user.email.includes('.')) {
-            return res.status(400).json({ error: 'O email é obrigatório e precisa ter ao menos 6 caracteres e conter @ e .' });
-        }
-
-        if (!user.password || user.password.length < 6) {
-            return res.status(400).json({ error: 'A senha é obrigatória e precisa ter ao menos 6 caracteres.' });
-        }
-
-        const userExist = await UserModel.findOne({email: user.email})
-
-        if(userExist) {
-            return res.status(400).json({ error: 'Já existe um usuário cadastrado com esse email.' });
-        }
-
-        const userToCreate = {
-            name: user.name,
-            email: user.email,
-            password: md5(user.password)
-        }
-        await UserModel.create(userToCreate);
-
-        return res.status(200).json({ msg: 'Usuário criado com sucesso!' });
-
+export const config = {
+    api: {
+        bodyParser: false,
     }
-    return res.status(405).json({ error: 'Metodo não permitido' });
 }
 
-export default connectDatabase(createUser);
+export default connectDatabase(handler);
